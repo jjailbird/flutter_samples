0958b445ec91c12887544011770633e726f1199b
jjailbird <jjailbird@gmail.com>
2022-06-16 14:33:01 +0900

    Change app title

--- base/lib/src/app.dart
+++ base/lib/src/app.dart
@@ -8,7 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "World Clock",
+      title: "Flutter Base",
       theme: getDefaultTheme(context),
       home: const SplashPage(),
     );    
