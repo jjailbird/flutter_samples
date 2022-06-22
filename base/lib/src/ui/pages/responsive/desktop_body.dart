b7a1920b539195a148edd98b0c637b350c032da2
jjailbird <jjailbird@gmail.com>
2022-06-22 15:49:33 +0900

    Update desktop_body.dart

--- base/lib/src/ui/pages/responsive/desktop_body.dart
+++ base/lib/src/ui/pages/responsive/desktop_body.dart
@@ -31,17 +31,17 @@ class DesktopBody extends StatelessWidget {
                   ),
                   Expanded(
                       child: ListView.builder(
-                    itemCount: 8,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          color: Colors.deepPurple[300],
-                          height: 120,
-                        ),
-                      );
-                    },
-                  )),
+                        itemCount: 8,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              color: Colors.deepPurple[300],
+                              height: 120,
+                            ),
+                          );
+                        },
+                      )),
                 ],
               ),
             ),
